0da050f8192de5c57d27f4097eb939162d9a24f9
Vihan226 <71296969+Vihan226@users.noreply.github.com>
2022-09-09 21:07:26 -0400

    Add files via upload

--- components/Rotation.js
+++ components/Rotation.js
@@ -1,87 +1,51 @@
-AFRAME.registerComponent("terrain-rotation-read", {
+AFRAME.registerComponent("diver-rotation-reader", {
+  schema: {
+    speedOfRoation: { type: "number", default: 0 },
+    speedOfAscent: { type: "number", default: 0 }
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
 
-schema:{
+      //get the data from the attributes
+      this.data.speedOfRoation = this.el.getAttribute("rotation");      
+      this.data.speedOfAscent = this.el.getAttribute("position");
 
-    speedOfRotation:{type:'number', default:0}
-},
+      var diverRotation = this.data.speedOfRoation;      
+      var diverPosition = this.data.speedOfAscent;
 
-
-tick:function(){
-
-    var mapRotation= this.el.getAttribute('rotation')
-    mapRotation.y +=this.data.speedOfRotation
-
-    this.el.setAttribute("rotation",{
-        x:mapRotation.x, y:mapRotation.y, z:mapRotation.z
-      })
-
-},
-
-
-init: function(){
-  
-    window.addEventListener('keydown', (e)=>{
-  
-      if(e.key==='ArrowRight'){
-        
-        if(this.data.speedOfRotation<.1){
-          this.data.speedOfRotation +=.01
+      //control the attributes with the Arrow Keys
+      if (e.key === "ArrowRight") {
+        if (diverRotation.x < 10) {
+          diverRotation.x += 0.5;
+          this.el.setAttribute("rotation", diverRotation);
         }
       }
-  
-      if(e.key==='ArrowLeft'){
-        
-        if(this.data.speedOfRotation>-.1){
-          this.data.speedOfRotation -=.01
+      if (e.key === "ArrowLeft") {
+        if (diverRotation.x > -10) {
+          diverRotation.x -= 0.5;
+          this.el.setAttribute("rotation", diverRotation);
         }
       }
-  
-  
-    })
-  
+      if (e.key === "ArrowUp") {
+        if (diverRotation.z < 20) {
+          diverRotation.z += 0.5;
+          this.el.setAttribute("rotation", diverRotation);
+        }
+        if (diverPosition.y < 2) {
+          diverPosition.y += 0.01;
+          this.el.setAttribute("position", diverPosition);
+        }
+      }
+      if (e.key === "ArrowDown") {
+        if (diverRotation.z > -10) {
+          diverRotation.z -= 0.5;
+          this.el.setAttribute("rotation", diverRotation);
+        }
+        if (diverPosition.y > -2) {
+          diverPosition.y -= 0.01;
+          this.el.setAttribute("position", diverPosition);
+        }
+      }
+    });
   }
-  
-  })
-
-
-
-
-  
-AFRAME.registerComponent('personRotationReader', {
-
-    schema:{
-      speedOfRotation:{type:"number",default:0}
-    },
-  
-    init: function(){
-      window.addEventListener('keydown', (e)=>{
-  
-        this.data.speedOfRotation=this.el.getAttribute('rotation')
-        var personRotation= this.data.speedOfRotation
-  
-  
-        if(e.key==='ArrowRight'){
-        
-          if(personRotation.x<10){
-            personRotation.x +=.5
-  
-            this.el.setAttribute('rotation', planeRotation)
-          }
-        }
-    
-        if(e.key==='ArrowLeft'){
-          
-          if(personRotation.x>-10){
-            personRotation.x-=.5
-  
-            this.el.setAttribute('rotation', rotation)
-          }
-        }
-  
-  
-      })
-    }
-    
-  })
-  
-  
+});
